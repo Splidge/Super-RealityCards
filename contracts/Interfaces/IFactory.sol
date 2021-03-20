75186dbd2133f6aa0299c88b58d2592247159b55
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 10:02:35 +0000

    wip

--- contracts/Interfaces/IFactory.sol
+++ contracts/Interfaces/IFactory.sol
@@ -4,5 +4,7 @@ pragma abicoder v2;
 
 interface IFactory {
     function initialize() external;
-    function createMarket() external;
+    function createMarket(uint256 _numberOfCards, uint256 marketFinishTime) external ;
+    function setAddresses(address _referenceMarket, address _referenceCard, address _sfHost, address _sfAgreements, address _daiSuperToken) external ;
+  
 }
