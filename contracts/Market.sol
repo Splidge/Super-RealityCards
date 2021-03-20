7727f1a458c527aeedfe198160964d843699b208
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 08:39:26 +0000

    init setup

---
+++ contracts/Market.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNDEFINED
+pragma solidity ^0.7.4;
+pragma abicoder v2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Initializable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract Market is Ownable {
+
+    constructor(){
+
+    }
+
+
+
+}
