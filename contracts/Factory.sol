4ede394436577660b4e8d9906f9ddf766e023829
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 10:02:02 +0000

    wip

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -8,13 +8,19 @@ import "./interfaces/IMarket.sol";
 
 contract Factory is Ownable {
 
-    address referenceMarket;
-    address referenceCard;
-    address[] markets;
+    address public referenceMarket;
+    address public referenceCard;
+    address[] public markets;
+    address public sfHost;
+    address public sfAgreements;
+    address public daiSuperToken;
 
-    constructor(address _referenceMarket, address _referenceCard){
+    constructor(address _referenceMarket, address _referenceCard, address _sfHost, address _sfAgreements, address _daiSuperToken){
         referenceMarket =_referenceMarket;
         referenceCard = _referenceCard;
+        sfHost = _sfHost;
+        sfAgreements = _sfAgreements;
+        daiSuperToken = _daiSuperToken;
     }
 
     function createMarket(uint256 _numberOfCards, uint256 marketFinishTime) external onlyOwner {
@@ -22,7 +28,16 @@ contract Factory is Ownable {
         markets.push(_marketAddress);
         IMarket _marketInstance = IMarket(_marketAddress);
         _marketInstance.initialize(referenceCard,_numberOfCards,marketFinishTime);
+    }
 
+
+    function setAddresses(address _referenceMarket, address _referenceCard, address _sfHost, address _sfAgreements, address _daiSuperToken) external onlyOwner {
+        // maybe we just use this instead of the constructor?
+        referenceMarket =_referenceMarket;
+        referenceCard = _referenceCard;
+        sfHost = _sfHost;
+        sfAgreements = _sfAgreements;
+        daiSuperToken = _daiSuperToken;
     }
 
 }
