9c6a5b61fc262990caa9573b4785f9c19b3ef11b
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 14:23:07 +0000

    wip

--- truffle-config.js
+++ truffle-config.js
@@ -96,7 +96,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.5",
+      version: "0.7.6",
       settings: {
         evmVersion: "istanbul",
         optimizer: {
