fa2f22f26817a615a979ee77c51efeb0179c9597
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 09:44:50 +0000

    wip

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,25 +3,23 @@ pragma solidity ^0.7.4;
 pragma abicoder v2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/proxy/Initializable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 import "./interfaces/IMarket.sol";
 
-contract Factory is Ownable, Initializable {
+contract Factory is Ownable {
 
     address referenceMarket;
     address referenceCard;
+    address[] markets;
 
     constructor(address _referenceMarket, address _referenceCard){
         referenceMarket =_referenceMarket;
         referenceCard = _referenceCard;
-
     }
-
-    function initialize() external initializer {}
 
     function createMarket(uint256 _numberOfCards, uint256 marketFinishTime) external onlyOwner {
         address _marketAddress = Clones.clone(referenceMarket);
+        markets.push(_marketAddress);
         IMarket _marketInstance = IMarket(_marketAddress);
         _marketInstance.initialize(referenceCard,_numberOfCards,marketFinishTime);
 
