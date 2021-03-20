4de5f8d259e242a9339fa3532bd82fbd201d3c09
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-20 13:51:54 +0000

    wip

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -14,6 +14,20 @@ contract Factory is Ownable {
     address public sfHost;
     address public sfAgreements;
     address public daiSuperToken;
+    address public zeroAddress = address(0);
+
+    event LogMarketCreated1(address contractAddress, address treasuryAddress, address nftHubAddress, uint256 referenceContractVersion);
+    event LogMarketCreated2(
+        address contractAddress,
+        uint32 mode,
+        string[] tokenURIs,
+        string ipfsHash,
+        uint32[] timestamps,
+        uint256 totalNftMintCount
+    );
+    event LogMarketApproved(address market, bool hidden);
+    event LogAdvancedWarning(uint256 _newAdvancedWarning);
+    event LogMaximumDuration(uint256 _newMaximumDuration);
 
     constructor(address _referenceMarket, address _referenceCard, address _sfHost, address _sfAgreements, address _daiSuperToken){
         referenceMarket =_referenceMarket;
@@ -28,6 +42,19 @@ contract Factory is Ownable {
         markets.push(_marketAddress);
         IMarket _marketInstance = IMarket(_marketAddress);
         _marketInstance.initialize(referenceCard,_numberOfCards,marketFinishTime);
+        string[] memory tokenURIs = new string[](20);
+        for(uint256 i; i <_numberOfCards; i++){
+            tokenURIs[i] = 'i';
+        }
+        uint32[] memory timestamps;
+        timestamps[0] = uint32(block.timestamp);
+        timestamps[1] = uint32(marketFinishTime);
+        timestamps[2] = uint32(marketFinishTime);
+        uint256 totalNftMintCount = _numberOfCards;
+        string memory ipfsHash = '0x0';
+        LogMarketCreated1(_marketAddress , zeroAddress, zeroAddress, 1);
+        LogMarketCreated2(_marketAddress, 0, tokenURIs, ipfsHash, timestamps, totalNftMintCount);
+
     }
 
 
