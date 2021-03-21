4f2cd8af0667f7680652701ba42c91022ee1e097
Andrew Stanger <andrew@stanger.org.uk>
2021-03-21 14:10:36 +0000

    remove warnings

--- contracts/Interfaces/INFTHub.sol
+++ contracts/Interfaces/INFTHub.sol
@@ -8,5 +8,6 @@ interface INFTHub {
     function transfer(address _from, address _to, uint256 _tokenId) external;
     function checkOwnerOf(uint256 _tokenId) external view returns(address owner);
     function checkBalanceOf(address _owner) external view returns(uint256 balance);
-    function totalTokens() external view returns(uint256 totalTokens);
+    function totalTokens() external view returns(uint256);
+
 }
